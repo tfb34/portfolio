f866258df09b883bb9b7fb30d27be6a29cce572c
Thalia <thalia.brito@rutgers.edu>
2018-06-07 00:47:57 -0400

    Add project logos

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function addHighlight(obj){
+	let x = document.getElementById(obj)
+	x.style.visibility = "visible"
+}
+
+function removeHighlight(obj){
+	let x = document.getElementById(obj)
+	x.style.visibility = "hidden"
+}
